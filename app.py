06254375f488f067d05c056101a20d13459ded61
Vlad <osadchiy900@icloud.com>
2026-08-29 17:12:23 +0300

    Fix typo in method parameter and clean up response structure in App class

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 class App:
-    async def __call__(self, scope, revieve, send):
-        if scope['type'] == 'http':
-            await send({
-                "type":   "http.response.start",
-                "status": 200,
-                "headers": [
-                    [
-                        b"content-type",
-                        b"text/plain"
-                     ],
-                ]
-            })
-            await send({
-                "type": "http.response.body",
-                "body": b"Hello from Taipan!"
-            })
-        if scope['type'] == 'lifespan':
+    async def __call__(self, scope, receive, send):
+        if scope["type"] == "http":
             await send({
                 "type": "http.response.start",
                 "status": 200,
-                "headers" : [
-                    [
-                        b"content-type",
-                        b"text/plain"
-                    ]
-                ]
+                "headers": [
+                    [b"content-type", b"text/plain"],
+                ],
             })
             await send({
-                "type": "http.reponse.body",
-                "body": b"Taipan Server was started!"
+                "type": "http.response.body",
+                "body": b"Hello from Taipan!",
             })
+
+        elif scope["type"] == "lifespan":
+            while True:
+                message = await receive()
+                if message["type"] == "lifespan.startup":
+                    await send({"type": "lifespan.startup.complete"})
+                elif message["type"] == "lifespan.shutdown":
+                    await send({"type": "lifespan.shutdown.complete"})
+                    return
